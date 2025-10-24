e531a7c743167a965734fd7ea5c9ed4e7cf9ba55
MarcYoba <yobamarc1@gmail.com>
2025-10-24 09:58:05 +0100

    creation de l'interface d'acceuil des utilisateur

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,13 +11,17 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email',EmailType::class,[
+                'attr' => ['class' => 'form-control form-control-user']
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -42,6 +46,18 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
+            ])
+            ->add('nom',TextType::class,[
+                'attr' => [
+                    'class' => 'form-control form-control-user',
+                    'placeholder'=> " Votre nom"
+                ]
+            ])
+            ->add('prenom',TextType::class,[
+                'attr' => [
+                    'class' => 'form-control form-control-user',
+                    'placeholder'=> " Votre prenom"
+                ]
             ])
         ;
     }
